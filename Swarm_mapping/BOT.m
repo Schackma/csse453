791e9852a361fd6ea87a117bc50597dc8a38c86a
Matthew <schackma@rose-hulman.edu>
2016-04-17 23:53:41 -0400

    returning works

--- Swarm_mapping/BOT.m
+++ Swarm_mapping/BOT.m
@@ -26,8 +26,8 @@ classdef BOT < handle
             [obj.sizex,obj.sizey] = size(glmap);
             obj.map = zeros(obj.sizex,obj.sizey);
             obj.map(motherPos(2),motherPos(1)) = -1;
-            obj.map = [ones(obj.sizey,1),obj.map,ones(obj.sizey,1)];
-            obj.map = [ones(1,obj.sizex+2);obj.map;ones(1,obj.sizex+2)];
+            obj.map = [ones(obj.sizex,1),obj.map,ones(obj.sizex,1)];
+            obj.map = [ones(1,obj.sizey+2);obj.map;ones(1,obj.sizey+2)];
             obj.mode = obj.EXPLORE;
             obj.currentPos = pos+1;
             obj.globalPos = pos;
@@ -41,15 +41,17 @@ classdef BOT < handle
                     deltaPos = obj.findClosestPoint();
                     obj.broadcastMessage = 'SEARCHING';
                 case obj.INFORM
-                    if size(obj.wayHome)==0
+                    if isempty(obj.wayHome)
                         deltaPos = obj.findPathHome();
+                        obj.wayHome = obj.wayHome(2:end,:);
                     else
                         deltaPos = obj.wayHome(1,:);
                         obj.wayHome = obj.wayHome(2:end,:);
                     end
                     obj.broadcastMessage = 'FOUND_VICTIM';
                 case obj.RETURN
-                    if size(obj.wayHome)==0
+                    size(obj.wayHome)
+                    if isempty(obj.wayHome)
                         deltaPos = obj.findPathHome();
                     else
                         deltaPos = obj.wayHome(1,:);
@@ -71,8 +73,8 @@ classdef BOT < handle
             initial = true;
             dx = 0;
             dy = 0;
-            while x ~=0 && y~=0
-                currentPoint = obj.map(y,x);
+            currentPoint = obj.map(y,x);
+            while currentPoint ~=0
                 if y+1<=obj.sizey && obj.map(y+1,x)~=99 && tempMap(y+1,x) ~=-1
                     if initial
                         possPoints = [possPoints(:,1),[y+1;x;obj.DOWN],possPoints(:,2:end)];
@@ -108,6 +110,11 @@ classdef BOT < handle
                 y = newData(1);
                 x = newData(2);
                 currentDirection = newData(3);
+                if(x==0 && y==0)
+                    break
+                end
+                currentPoint = obj.map(y,x);
+                
                 possPoints = possPoints(:,1:end-1);
             end
             switch currentDirection
@@ -143,18 +150,20 @@ classdef BOT < handle
             tempMap(y,x)=-1;
             while currentPoint ~=-1
                if y+1<obj.sizey && obj.map(y+1,x)~=99 && tempMap(y+1,x) ~=2
-                   possPoints = [possPoints(:,1),[y+1;x;obj.UP],possPoints(:,2:end)];
+                   possPoints = [possPoints(:,1),[y+1;x;obj.DOWN],possPoints(:,2:end)];
                end
                if y-1>0 && obj.map(y-1,x)~=99 && tempMap(y-1,x) ~=2
-                   possPoints = [possPoints(:,1),[y-1;x;obj.DOWN],possPoints(:,2:end)];
+                   possPoints = [possPoints(:,1),[y-1;x;obj.UP],possPoints(:,2:end)];
                end
                if x+1<obj.sizex &&obj.map(y,x+1)~=99 && tempMap(y,x+1) ~=2
-                   possPoints = [possPoints(:,1),[y;x+1;obj.LEFT],possPoints(:,2:end)];
+                   possPoints = [possPoints(:,1),[y;x+1;obj.RIGHT],possPoints(:,2:end)];
                end
                if x-1>0 && obj.map(y,x-1)~=99&& tempMap(y,x-1) ~=2
-                   possPoints = [possPoints(:,1),[y;x-1;obj.RIGHT],possPoints(:,2:end)];
+                   possPoints = [possPoints(:,1),[y;x-1;obj.LEFT],possPoints(:,2:end)];
                end
-                visited = [visited,[y;x;currentDirection]];
+               if tempMap(x,y) ~=1
+                    visited = [visited,[y;x;currentDirection]];
+               end
                 newData = possPoints(:,end);
                 y = newData(1);
                 x = newData(2);
@@ -164,30 +173,32 @@ classdef BOT < handle
                 possPoints = possPoints(:,1:end-1);
             end
             [vizX,vizY] = size(visited);
-            while x~=currx && y~=curry
+            while x~=currx || y~=curry
                 [dx,dy] = obj.moveWay(currentDirection);
                 x = x+dx;
                 y = y+dy;
-                check = [ones(1,vizY).*x;ones(1,vizY).*y];
+                check = [ones(1,vizY).*y;ones(1,vizY).*x];
+                find((sum((visited(1:2,:) == check)) == 2) == 1)
                 currentDirection = visited(3,find((sum((visited(1:2,:) == check)) == 2) == 1));
                 obj.wayHome = [dx,dy;obj.wayHome];
             end
-            obj.wayHome(2:end,:);
-            output = [dx,dy];
+            obj.wayHome =obj.wayHome *-1;
+            obj.wayHome = obj.wayHome(2:end,:);
+            output = [-dx,-dy];
         end
         
         function [x,y] = moveWay(obj,dir)
             switch dir
-                case obj.UP
-                    x = 0;
-                    y = -1;
                 case obj.DOWN
                     x = 0;
+                    y = -1;
+                case obj.UP
+                    x = 0;
                     y = 1;
-                case obj.LEFT
+                case obj.RIGHT
                     x = -1;
                     y = 0;
-                case obj.RIGHT
+                case obj.LEFT
                     x = 1;
                     y = 0;
             end
